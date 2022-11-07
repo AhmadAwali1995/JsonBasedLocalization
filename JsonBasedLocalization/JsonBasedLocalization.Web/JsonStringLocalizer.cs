0ccdd5e4b466e40e492925b6fe249b58de6ae621
AhmadAwali1995 <112668126+AhmadAwali1995@users.noreply.github.com>
2022-11-07 23:59:44 +0200

    Add files via upload

---
+++ JsonBasedLocalization/JsonBasedLocalization.Web/JsonStringLocalizer.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Localization;
+using Newtonsoft.Json;
+
+namespace JsonBasedLocalization.Web
+{
+    public class JsonStringLocalizer : IStringLocalizer
+    {
+        private readonly JsonSerializer _Serializer = new JsonSerializer();
+        private readonly IDistributedCache _Cache;
+
+        public JsonStringLocalizer(IDistributedCache cache)
+        {
+            _Cache = cache;
+        }
+        public LocalizedString this[string name]
+        {
+            get
+            {
+                var value = GetString(name);
+                return new LocalizedString(name, value);
+            }
+        }
+
+        public LocalizedString this[string name, params object[] arguments]
+        {
+            get
+            {
+                var actualValue = this[name];
+                return !actualValue.ResourceNotFound 
+                    ? new LocalizedString(name, String.Format(actualValue.Value, arguments)) 
+                    : actualValue;
+            }
+        }
+
+        public IEnumerable<LocalizedString> GetAllStrings(bool includeParentCultures)
+        {
+            var filePath = $"Resources/{Thread.CurrentThread.CurrentCulture.Name}.json";
+            using FileStream stream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+            using StreamReader sr = new StreamReader(stream);
+            using JsonTextReader reader = new JsonTextReader(sr);
+
+            while (reader.Read())
+            {
+                if (reader.TokenType == JsonToken.PropertyName)
+                    continue;
+
+                var key = reader.Value as string;
+                reader.Read();
+
+                var value = _Serializer.Deserialize<string>(reader);
+                yield return new LocalizedString(key, value);
+            }
+
+
+        }
+
+        private string GetString(string Key)
+        {
+            var filePath = $"Resources/{Thread.CurrentThread.CurrentCulture.Name}.json";
+            var fullFilePath = Path.GetFullPath(filePath);
+
+            if (File.Exists(fullFilePath))
+            {
+                var casheKey = $"locale_{Thread.CurrentThread.CurrentCulture.Name}_{Key}";
+                var casheValue = _Cache.GetString(casheKey);
+
+                if(!string.IsNullOrEmpty(casheValue))
+                {
+                    return casheValue;
+                }
+
+                var result = GetValueFromJson(Key, fullFilePath);
+
+                if (!string.IsNullOrEmpty(result))
+                {
+                    _Cache.SetString(casheKey, result);
+                }
+                return result;
+            }
+
+            return String.Empty;
+        }
+
+        private string GetValueFromJson(string PropertyName, string FilePath)
+        {
+            if(string.IsNullOrEmpty(FilePath) || string.IsNullOrEmpty(FilePath))
+                return string.Empty;
+
+            using FileStream stream = new FileStream(FilePath, FileMode.Open, FileAccess.Read);
+            using StreamReader sr = new StreamReader(stream);
+            using JsonTextReader reader = new JsonTextReader(sr);
+
+            while (reader.Read())
+            {
+                if(reader.TokenType == JsonToken.PropertyName && reader.Value as string == PropertyName)
+                {
+                    reader.Read();
+                    return _Serializer.Deserialize<string>(reader);
+                }
+            }
+
+            return String.Empty;
+        }
+    }
+}
---
+++ JsonBasedLocalization/JsonBasedLocalization.Web/Models/CreateTestViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JsonBasedLocalization.Web.Models
+{
+    public class CreateTestViewModel
+    {
+        [Display(Name="name"), Required(ErrorMessage ="required")]
+        public string Name { get; set; }
+    }
+}
